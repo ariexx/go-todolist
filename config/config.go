6e5bf008ca3fc81b8512cc16979faece17dc8f95
Muhammad Arief <mgi.official1@gmail.com>
2023-06-23 17:50:42 +0700

    get port from config

--- config/config.go
+++ config/config.go
@@ -3,9 +3,11 @@ package config
 import "go-todolist/helper"
 
 var (
-	DbUser = helper.SetConfig("database.username", "")
-	DbPass = helper.SetConfig("database.password", "")
-	DbHost = helper.SetConfig("database.host", "")
-	DbPort = helper.SetConfig("database.port", "")
-	DbName = helper.SetConfig("database.name", "")
+	AppName = helper.SetConfig("appName", "")
+	AppPort = helper.SetConfig("server.port", "")
+	DbUser  = helper.SetConfig("database.username", "")
+	DbPass  = helper.SetConfig("database.password", "")
+	DbHost  = helper.SetConfig("database.host", "")
+	DbPort  = helper.SetConfig("database.port", "")
+	DbName  = helper.SetConfig("database.name", "")
 )
--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"github.com/gofiber/fiber/v2"
 	"github.com/sirupsen/logrus"
 	"go-todolist/config"
@@ -11,15 +10,13 @@ func main() {
 	db := config.DbConn
 	defer config.CloseConnection(db)
 
-	port := fmt.Sprintf(":%d", 8888)
-
 	app := fiber.New()
 
 	app.Get("/hello", func(c *fiber.Ctx) error {
 		return c.Send([]byte("Anjayy"))
 	})
 
-	err := app.Listen(port)
+	err := app.Listen(":" + config.AppPort)
 	if err != nil {
 		config.NewLoggerService().SaveLog(logrus.ErrorLevel, err)
 		panic(err)
