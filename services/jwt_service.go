b3b53001038c716ca435757ffe5c9ae3f8412400
Muhammad Arief <mgi.official1@gmail.com>
2023-06-27 06:54:33 +0700

    add validateToken in jwt_service.go

--- services/jwt_service.go
+++ services/jwt_service.go
@@ -1,5 +1,8 @@
 package services
 
+import "github.com/golang-jwt/jwt/v5"
+
 type JwtService interface {
 	GenerateToken(userId uint) (string, error)
+	ValidateToken(token string) (*jwt.Token, error)
 }
--- services/jwt_service_impl.go
+++ services/jwt_service_impl.go
@@ -1,11 +1,29 @@
 package services
 
 import (
+	"errors"
 	"github.com/golang-jwt/jwt/v5"
 	"go-todolist/config"
 )
 
 type jwtServiceImpl struct {
+}
+
+func (j *jwtServiceImpl) ValidateToken(token string) (*jwt.Token, error) {
+	var secretKey = []byte(config.JwtSecret)
+	newToken, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
+		if !ok {
+			return nil, jwt.ErrSignatureInvalid
+		}
+		return secretKey, nil
+	})
+
+	if err != nil {
+		return nil, errors.New("invalid token")
+	}
+
+	return newToken, nil
 }
 
 func NewJwtService() JwtService {
